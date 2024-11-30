da39aa174a450f440cb8bb75fc1ac660f8e4efde
Muhammad-abdullah7 <mabdullahzafar007@gmail.com>
2024-11-30 16:19:57 +0500

    Create task9.cpp

---
+++ lab manual 12/task9.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+char obj[5][5];
+bool gravity=false;
+bool isblackhole=false;
+void displayworld() {
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            cout<<obj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+}
+void setgravtatus(bool status){
+    gravity=status;
+}
+void setblholeStatus(bool status){
+    isblackhole=status;
+}
+void timeTick(int times){
+    if(!gravity) return;
+    for(int tick=0;tick<times;tick++){
+        for(int r=4;r>0;r--){
+            for(int c=0;c<5;c++){
+                if(obj[r-1][c]=='#'&&obj[r][c]=='-'){
+                    obj[r][c]='#';
+                    obj[r-1][c]='-';
+                }
+                else if(isblackhole&&r==4&&objects[r][c]=='#'){
+                    obj[r][c]='-';
+                    obj[0][c]='#';
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    char initial[5][5]={
+        {'-', '#', '#', '-', '#'},
+        {'#', '-', '-', '#', '-'},
+        {'-', '#', '-', '-', '-'},
+        {'#', '-', '#', '-', '#'},
+        {'#', '-', '-', '-', '-'}
+    };
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            obj[i][j]=initial[i][j];
+        }
+    }
+    cout<<"Initial World:"<<endl;
+    displayworld();
+    setgravStatus(true);
+    setblholeStatus(true);
+    timeTick(3);
+    cout<<"After three Gravity Ticks with Black Hole:"<<endl;
+    displayworld();
+    return 0;
+}
